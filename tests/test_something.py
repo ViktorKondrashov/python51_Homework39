2b1e72a5d3466684ab9d7ebc29b2a69c3485e48c
aabuiluk <andrii.a.builuk@gmail.com>
2025-06-24 21:22:46 +0300

    Init

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,16 +1,16 @@
 from main import *
 
 
-def test_add():
+def test_add1():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
 
 
-def test_subtract():
+def test_subtract1():
     assert subtract(10, 4) == 6
     assert subtract(0, 5) == -5
 
 
-def test_multiply():
+def test_multiply1():
     assert multiply(3, 4) == 12
     assert multiply(-2, 5) == -10
